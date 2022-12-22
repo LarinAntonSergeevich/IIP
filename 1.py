10e5d946f0c3518b0b76091c7491b384b2df7ba6
ТыгыДык21 <112696189+LarinAntonSergeevich@users.noreply.github.com>
2022-12-23 01:27:35 +0500

    is

--- 1.py
+++ 1.py
@@ -1,5 +1,15 @@
-a = list(map(int, input().split()))
-s = 0
-for el in a:
-    s += el
-print(s)
+a=int(input("ОТДАЙ СВОЁ ЧИСЛО!!!"))
+
+b=int(input("ГДЕ МОЁ ЧИСЛО!!!"))
+
+if a==b:
+
+   print("ОНИ РАВНЫ КАК ТАК-ТО")
+
+elif a>b:
+
+   print(a,"Больше-",b)
+
+elif a<b:
+
+   print(b,"Больше_",a)
