421129b557fd6b82e2fd9959672eaaa9a7721b19
ТыгыДык21 <112696189+LarinAntonSergeevich@users.noreply.github.com>
2022-12-23 01:38:25 +0500

    123

--- 6.py
+++ 6.py
@@ -1,9 +1,3 @@
-def getSum(n):
-    
-    sum = 0
-    for digit in str(n): 
-      sum += int(digit)      
-    return sum
-   
-n = 12345
-print(getSum(n))
+a = int(input('number here.........'))
+s = [int(x) for x in str(a)]
+print(sum(s))
